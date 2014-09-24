aba17e606607b3378027ab92816cf7c3180ea0a7
Da Moose <ahmed.musawir@hotmail.com>
2014-09-24 14:15:07 +0800

    setup done

--- js/form.js
+++ js/form.js
@@ -2,6 +2,51 @@
 
 	$('html').addClass('js');
 
+	contactForm = {
+
+		formContainer: $('#contact'),
+		
+		init: function (conf) {
+			
+			$('<a/>', {
+				text: "Push Me",
+				'class': "btn btn-danger",
+				'id': "myButton",
+				'href': "http://google.com" 
+			})
+			.insertAfter('article')
+			.on('click', this.show);
+		},
+
+		show: function (event) {
+			// console.log('showing ...');
+			event.preventDefault();
+
+			var $self = contactForm;
+
+			// $self.close();
+			
+			$self.close.call($self.formContainer);
+
+			$self.formContainer.show();
+
+			// $self.call.close();
+			
+		},
+
+		close: function (event) {
+			
+			$('<span/>', {
+				text: "X",
+				'class': "close"
+			})
+				.prependTo(this);
+
+			// console.log(this);
+		}
+
+	}
+
 
 
 })();
